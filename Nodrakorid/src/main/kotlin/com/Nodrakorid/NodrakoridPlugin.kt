d98767e6a86006b7943ff758559605e85e6b6b49
TeKuma <106915307+TeKuma25@users.noreply.github.com>
2025-02-13 19:29:55 +0700

    Update NodrakoridPlugin.kt

--- Nodrakorid/src/main/kotlin/com/Nodrakorid/NodrakoridPlugin.kt
+++ Nodrakorid/src/main/kotlin/com/Nodrakorid/NodrakoridPlugin.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class GomovPlugin : Plugin() {
+class Nodrakorid : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list
         // directly.
