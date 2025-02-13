e3a43561a3c4c3f2bea9c864c21a42a521e2737a
TeKuma <106915307+TeKuma25@users.noreply.github.com>
2025-02-13 19:14:50 +0700

    Update build.gradle.kts

--- Dubbindo/build.gradle.kts
+++ Dubbindo/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-     authors = listOf("TeKuma25")
+     authors = listOf("Hexated")
 
     /**
      * Status int as the following:
