6630998ab296cfc4c3466de6bc797a9501af99cb
TeKuma <106915307+TeKuma25@users.noreply.github.com>
2025-02-13 19:31:06 +0700

    Update PusatfilmPlugin.kt

--- Pusatfilm/src/main/kotlin/com/Pusatfilm/PusatfilmPlugin.kt
+++ Pusatfilm/src/main/kotlin/com/Pusatfilm/PusatfilmPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class GomovPlugin : Plugin() {
+class PusatfilmPlugin : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list
         // directly.
