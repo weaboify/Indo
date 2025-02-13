f80a0e27c811e2727031df7d33561d9e7253c91a
TeKuma <106915307+TeKuma25@users.noreply.github.com>
2025-02-13 19:32:11 +0700

    Update DutamoviePlugin.kt

--- Dutamovie/src/main/kotlin/com/Dutamovie/DutamoviePlugin.kt
+++ Dutamovie/src/main/kotlin/com/Dutamovie/DutamoviePlugin.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class DutaMoviePlugin : Plugin() {
+class DutamoviePlugin : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list
         // directly.
