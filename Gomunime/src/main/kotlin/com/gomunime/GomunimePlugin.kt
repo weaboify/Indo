635f588a46bc6bf59d37469cdbfa5871f08baec9
tkusuma01 <teja_253@yahoo.com>
2025-02-21 00:41:13 +0700

    upd

--- Gomunime/src/main/kotlin/com/gomunime/GomunimePlugin.kt
+++ Gomunime/src/main/kotlin/com/gomunime/GomunimePlugin.kt
@@ -9,6 +9,6 @@ import android.content.Context
 class GomunimePlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(GomunimeProvider())
+        registerMainAPI(Gomunime())
     }
 }
