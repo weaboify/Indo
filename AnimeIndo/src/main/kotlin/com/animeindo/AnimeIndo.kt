704cd47eec2b00d26f2480a4714932ffb42766ba
tkusuma01 <teja_253@yahoo.com>
2025-02-26 15:19:09 +0700

    update animeindo

--- AnimeIndo/src/main/kotlin/com/animeindo/AnimeIndo.kt
+++ AnimeIndo/src/main/kotlin/com/animeindo/AnimeIndo.kt
@@ -63,7 +63,7 @@ class AnimeIndo : MainAPI() {
                     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl/page/$page/?s=$query").document
+        val document = aapp.get("$mainUrl/${request.data}/page/$page").document
         val home = document.select("article.bs").mapNotNull { it.toSearchResult() }
         return newHomePageResponse(request.name, home)
     }
