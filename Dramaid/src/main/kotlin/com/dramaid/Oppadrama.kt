f592d4d70096aa1a54fe43d7eecabea07a398d96
jkperm144 <jokopermana144@gmail.com>
2025-04-02 00:34:28 +0700

    Update Oppadrama.kt

--- Dramaid/src/main/kotlin/com/dramaid/Oppadrama.kt
+++ Dramaid/src/main/kotlin/com/dramaid/Oppadrama.kt
@@ -3,6 +3,6 @@ package com.dramaid
 import com.lagradost.cloudstream3.extractors.Filesim
 
 class Oppadrama : Dramaid() {
-    override var mainUrl = "http://45.11.57.64"
+    override var mainUrl = "http://45.11.57.118"
     override var name = "Oppadrama"
 }
